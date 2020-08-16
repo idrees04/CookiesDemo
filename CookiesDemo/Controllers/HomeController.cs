27c91816debcaee30d15e0b0437cd189d5290253
idrees04 <idreesar@yahoo.com>
2020-08-16 17:06:49 +0500

    Add project files.

---
+++ CookiesDemo/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using CookiesDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CookiesDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(User u)
+        {
+
+            if(ModelState.IsValid==true)
+            {
+                HttpCookie cookie = new HttpCookie("Username");
+                cookie.Value = u.Username;
+
+                HttpContext.Response.Cookies.Add(cookie);
+                //to make Persistent cookie wil add this line
+                //cookie.Expires = DateTime.Now.AddDays(2);
+                cookie.Expires = DateTime.Now.AddMinutes(2);
+                return RedirectToAction("Index","Dashboard");
+            }
+            return View(u);
+        }
+    }
+}
---
+++ CookiesDemo/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CookiesDemo.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage ="UserName is Required")]
+        public string Username { get; set; }
+    }
+}
